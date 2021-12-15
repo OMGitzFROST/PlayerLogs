d35906cc46846f4d85d5fffe8b8935ae878742ce
OMGitzFROST <deltoro.steve98@gmail.com>
2021-12-14 19:19:00 -0800

    Added Activity.java

---
+++ src/main/java/com/frostdeveloper/playerlog/Activity.java
@@ -0,0 +1,18 @@
+package com.frostdeveloper.playerlog;
+
+/**
+ * An enum used to define our activity types
+ *
+ * @since 1.0
+ */
+public enum Activity {
+	ALL,
+	PLAYER_CHAT,
+	PLAYER_COMMAND,
+	PLAYER_JOIN,
+	PLAYER_QUIT,
+	PLAYER_DEATH,
+	WORLD_CHANGE,
+	BLOCK_BREAK,
+	BLOCK_PLACE
+}
