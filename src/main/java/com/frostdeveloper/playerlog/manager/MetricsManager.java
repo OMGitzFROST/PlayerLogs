20f06de52d2be0b94101e5b358190baa2849cbd8
OMGitzFROST <deltoro.steve98@gmail.com>
2021-12-15 10:07:50 -0800

    Metrics runs on a scheduler

--- src/main/java/com/frostdeveloper/playerlog/manager/MetricsManager.java
+++ src/main/java/com/frostdeveloper/playerlog/manager/MetricsManager.java
@@ -3,6 +3,7 @@ package com.frostdeveloper.playerlog.manager;
 import com.frostdeveloper.playerlog.PlayerLog;
 import org.bstats.bukkit.Metrics;
 import org.bstats.charts.SimplePie;
+import org.bukkit.Bukkit;
 
 /**
  * A class used to handle collection of metrics and server demographics.
@@ -24,19 +25,21 @@ public class MetricsManager
 	 */
 	public void runTask()
 	{
-		if (config.getBoolean(ConfigManager.Config.USE_METRICS)) {
-			int id = 13598;
-			Metrics metrics = new Metrics(plugin, id);
-			
-			metrics.addCustomChart(new SimplePie("locale", () -> config.getString(ConfigManager.Config.LOCALE)));
-			metrics.addCustomChart(new SimplePie("auto_updater", () -> config.getString(ConfigManager.Config.AUTO_UPDATE)));
-			metrics.addCustomChart(new SimplePie("use_prefix", () -> config.getString(ConfigManager.Config.USE_PREFIX)));
-			metrics.addCustomChart(new SimplePie("use_custom_message", () -> config.getString(ConfigManager.Config.CUSTOM_MESSAGE)));
-			
-			plugin.log(getClass(), "metrics.collect.success");
-		}
-		else {
-			plugin.log(getClass(), "metrics.collect.disabled");
-		}
+		Bukkit.getScheduler().runTaskLater(plugin, () -> {
+			if (config.getBoolean(ConfigManager.Config.USE_METRICS)) {
+				int id = 13598;
+				Metrics metrics = new Metrics(plugin, id);
+				
+				metrics.addCustomChart(new SimplePie("locale", () -> config.getString(ConfigManager.Config.LOCALE)));
+				metrics.addCustomChart(new SimplePie("auto_updater", () -> config.getString(ConfigManager.Config.AUTO_UPDATE)));
+				metrics.addCustomChart(new SimplePie("use_prefix", () -> config.getString(ConfigManager.Config.USE_PREFIX)));
+				metrics.addCustomChart(new SimplePie("use_custom_message", () -> config.getString(ConfigManager.Config.CUSTOM_MESSAGE)));
+				
+				plugin.log(MetricsManager.class, "metrics.collect.success");
+			}
+			else {
+				plugin.log(MetricsManager.class, "metrics.collect.disabled");
+			}
+		}, 0);
 	}
 }
