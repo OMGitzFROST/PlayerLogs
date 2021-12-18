3afedc45cc4897786cb9c1ba07246c7eab2748bb
OMGitzFROST <deltoro.steve98@gmail.com>
2021-12-17 16:30:01 -0800

    Registered features and addeed JavaDocs

--- src/main/java/com/frostdeveloper/playerlog/PlayerLog.java
+++ src/main/java/com/frostdeveloper/playerlog/PlayerLog.java
@@ -1,19 +1,35 @@
 package com.frostdeveloper.playerlog;
 
 import com.frostdeveloper.api.FrostAPI;
+import com.frostdeveloper.playerlog.command.BaseCommand;
 import com.frostdeveloper.playerlog.listener.ActivityListener;
+import com.frostdeveloper.playerlog.listener.JoinListener;
 import com.frostdeveloper.playerlog.manager.*;
 import com.frostdeveloper.playerlog.util.LogFilter;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
 import java.util.logging.Level;
 
+/**
+ * A class defined as this plugin's main class, It's used to handle and execute all
+ * methods and features found within all classes.
+ *
+ * @author OMGitzFROSt
+ * @since 1.0
+ */
 public final class PlayerLog extends JavaPlugin
 {
+	// CLASS INSTANCE
 	private static PlayerLog instance;
 	
+	/**
+	 * A method invoked on plugin enable.
+	 *
+	 * @since 1.0
+	 */
 	@Override
 	public void onEnable()
 	{
@@ -23,17 +39,28 @@ public final class PlayerLog extends JavaPlugin
 			getUpdateManager().runTask();
 			getMetricsManager().runTask();
 			
+			// CREATE FILES
 			getConfigManager().createFile();
 			getLocaleManager().createFile();
 			
+			// REGISTER LISTENERS
 			getServer().getPluginManager().registerEvents(new ActivityListener(), this);
+			getServer().getPluginManager().registerEvents(new JoinListener(), this);
+			
+			// REGISTER COMMANDS
+			Objects.requireNonNull(getCommand("playerlog")).setExecutor(new BaseCommand());
 			
 			log("plugin.enable.success", getDescription().getVersion());
 		}
 		catch (Exception ex) {
+			log("plugin.enable.failed", getDescription().getVersion());
 			ReportManager.createReport(ex, true);
 		}
 	}
+	
+	/*
+	 * STANDARD LOGGERS
+	 */
 	
 	/**
 	 * A method used to send logging messages to the console.
@@ -94,6 +121,14 @@ public final class PlayerLog extends JavaPlugin
 	 */
 	
 	/**
+	 * A method used to log a debug message to the console.
+	 *
+	 * @param msg Object instance of a message
+	 * @since 1.1
+	 */
+	public void debug(Object msg) { getLogger().log(Level.INFO, "[DEBUG] " + msg); }
+	
+	/**
 	 * A method used to log debug messages to the console.
 	 *
 	 * @param key   Message key
@@ -151,23 +186,65 @@ public final class PlayerLog extends JavaPlugin
 	 * CLASS INSTANCE GETTERS
 	 */
 	
+	/**
+	 * A method used to return an instance of the main class
+	 *
+	 * @return Main class
+	 * @since 1.0
+	 */
 	public static PlayerLog getInstance()            { return instance; }
 	
+	/**
+	 * A method used to return an instance of the FrostAPI class
+	 *
+	 * @return FrostAPI class
+	 * @since 1.0
+	 */
 	@Contract (value = " -> new", pure = true)
 	public @NotNull FrostAPI getFrostApi()           { return new FrostAPI(this); }
 	
+	/**
+	 * A method used to return an instance of the LogManager class
+	 *
+	 * @return LogManager class
+	 * @since 1.0
+	 */
 	@Contract (" -> new")
 	public @NotNull LogManager getLogManager()       { return new LogManager();            }
 	
+	/**
+	 * A method used to return an instance of the ConfigManager class
+	 *
+	 * @return ConfigManager class
+	 * @since 1.0
+	 */
 	@Contract (" -> new")
 	public @NotNull ConfigManager getConfigManager()   { return new ConfigManager();         }
 	
+	/**
+	 * A method used to return an instance of the LocaleManager class
+	 *
+	 * @return LocaleManager class
+	 * @since 1.0
+	 * */
 	@Contract (" -> new")
 	public @NotNull LocaleManager getLocaleManager()   { return new LocaleManager();         }
 	
+	/**
+	 * A method used to return an instance of the MetricsManager class
+	 *
+	 * @return MetricsManager class
+	 * @since 1.0
+	 */
 	@Contract (" -> new")
 	public @NotNull MetricsManager getMetricsManager() { return new MetricsManager();        }
 	
+	/**
+	 * A method used to return an instance of the UpdateManager class
+	 *
+	 * @return UpdateManager class
+	 * @since 1.0
+	 */
 	@Contract (" -> new")
 	public @NotNull UpdateManager getUpdateManager()    { return new UpdateManager();        }
 }
