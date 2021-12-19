e20e495b1a6ccb1bdc93f6364bf53fc2349fd7ca
OMGitzFROST <deltoro.steve98@gmail.com>
2021-12-19 02:44:07 -0800

    Removed unused method

--- src/main/java/com/frostdeveloper/playerlog/manager/CacheManager.java
+++ src/main/java/com/frostdeveloper/playerlog/manager/CacheManager.java
@@ -8,13 +8,28 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * A class used to manage our cache. This mananger includes methods to set, get, and delete cache values.
+ *
+ * @author OMGitzFROST
+ * @since 1.1
+ */
 public class CacheManager
 {
+	// CLASS INSTANCES
 	private final PlayerLog plugin = PlayerLog.getInstance();
 	private final FrostAPI api = plugin.getFrostApi();
 	
+	// CLASS SPECIFIC OBJECTS
 	private final File cacheFile = api.toFile( ".cache.yml");
 	
+	/**
+	 * A method used to set a cache to a value
+	 *
+	 * @param path Target cache
+	 * @param value Target value
+	 * @since 1.1
+	 */
 	public void setCache(String path, Object value)
 	{
 		try {
@@ -27,6 +42,13 @@ public class CacheManager
 		}
 	}
 	
+	/**
+	 * A method used to get a cache value/
+	 *
+	 * @param path Target path
+	 * @return Cache value
+	 * @since 1.1
+	 */
 	public String getCache(String path)
 	{
 		if (cacheFile.exists()) {
@@ -40,19 +62,15 @@ public class CacheManager
 		}
 	}
 	
+	/**
+	 * A method used to delete a cache path, in other words, used to reset its value.
+	 *
+	 * @param path Target path
+	 * @since 1.1
+	 */
 	public void deleteCache(String path)
 	{
 		FileConfiguration config = YamlConfiguration.loadConfiguration(cacheFile);
 		config.set(path, null);
-	}
-	
-	public void delete()
-	{
-		if (cacheFile.delete()) {
-			plugin.log("cache.delete.success");
-		}
-		else {
-			plugin.log("cache.delete.failed");
-		}
 	}
 }
