99df4504a421094b83c174d9077bd44dacebdcf1
OMGitzFROST <deltoro.steve98@gmail.com>
2021-12-27 01:25:34 -0800

    Added reload and reformatted class

--- src/main/java/com/frostdeveloper/playerlogs/manager/ConfigManager.java
+++ src/main/java/com/frostdeveloper/playerlogs/manager/ConfigManager.java
@@ -55,16 +55,20 @@ public class ConfigManager
 	}
 	
 	/**
+	 * A method used to reload our configuration file
+	 *
+	 * @since 1.2
+	 */
+	public void reload()                            { yaml.reload();                                            }
+	
+	/**
 	 * A method used to return a string from our configuration
 	 *
 	 * @param path Configuration path
 	 * @return Configuration string
 	 * @since 1.0
 	 */
-	public String getString(@NotNull Config path)
-	{
-		return yaml.getString(path.getKey(), path.getDefault());
-	}
+	public String getString(@NotNull Config path)   { return yaml.getString(path.getKey(), path.getDefault());  }
 	
 	/**
 	 * A method used to return a boolean value from our configuration, it takes a string and parses it into
@@ -74,10 +78,7 @@ public class ConfigManager
 	 * @return Parsed boolean
 	 * @since 1.0
 	 */
-	public boolean getBoolean(@NotNull Config path)
-	{
-		return yaml.getBoolean(path.getKey(), path.getDefault());
-	}
+	public boolean getBoolean(@NotNull Config path) { return yaml.getBoolean(path.getKey(), path.getDefault()); }
 	
 	/**
 	 * A method used to return a double value from our configuration, it takes a string and parses it into
@@ -87,8 +88,5 @@ public class ConfigManager
 	 * @return Parsed double
 	 * @since 1.1
 	 */
-	public double getDouble(@NotNull Config path)
-	{
-		return yaml.getDouble(path.getKey(), path.getDefault());
-	}
+	public double getDouble(@NotNull Config path)   { return yaml.getDouble(path.getKey(), path.getDefault());  }
 }
