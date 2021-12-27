38424e19afd99ea9f0b17361d495aa9c0a8b9ca0
OMGitzFROST <deltoro.steve98@gmail.com>
2021-12-27 14:50:50 -0800

    Migrated to properties from yml

--- src/main/java/com/frostdeveloper/playerlogs/manager/CacheManager.java
+++ src/main/java/com/frostdeveloper/playerlogs/manager/CacheManager.java
@@ -1,7 +1,13 @@
 package com.frostdeveloper.playerlogs.manager;
 
+import com.frostdeveloper.api.FrostAPI;
+import com.frostdeveloper.api.core.Properties;
 import com.frostdeveloper.api.core.Yaml;
+import com.frostdeveloper.playerlogs.PlayerLogs;
 import com.frostdeveloper.playerlogs.util.Util;
+import org.bukkit.entity.Player;
+
+import java.io.File;
 
 /**
  * A class used to manage our cache. This mananger includes methods to set, get, and delete cache values.
@@ -12,7 +18,8 @@ import com.frostdeveloper.playerlogs.util.Util;
 public class CacheManager
 {
 	// CLASS SPECIFIC OBJECTS
-	private final Yaml cache = new Yaml(Util.toFile( ".cache.yml"), true);
+	private final Properties cache = new Properties(true);
+	private final File cacheFile   = Util.toFile(".cache.properties");
 
 	/**
 	 * A method used to set a cache to a value
@@ -21,8 +28,26 @@ public class CacheManager
 	 * @param value Target value
 	 * @since 1.1
 	 */
-	public void setCache(String path, Object value) { cache.setDefault(path, value, true);      }
-
+	public void setCache(String path, Object value)
+	{
+		getCache().setProperty(path, value, true);
+		cache.store(cacheFile);
+	}
+	
+	/**
+	 * This method is used to load and return our cache properties
+	 *
+	 * @return Cache map
+	 * @since 1.2
+	 */
+	public Properties getCache()
+	{
+		if (cacheFile.exists()) {
+			cache.load(cacheFile);
+		}
+		return cache;
+	}
+	
 	/**
 	 * A method used to get a cache value/
 	 *
@@ -30,7 +55,16 @@ public class CacheManager
 	 * @return Cache value
 	 * @since 1.1
 	 */
-	public String getCache(String path)             { return cache.getConfig().getString(path); }
+	public String getCache(String path, Object def) { return getCache().getProperty(path, def); }
+	
+	/**
+	 * A method used to return a value from a cached path
+	 *
+	 * @param path Target path
+	 * @return Cached value
+	 * @since 1.2
+	 */
+	public String getCache(String path)             { return getCache().getProperty(path); }
 
 	/**
 	 * A method used to delete a cache path, in other words, used to reset its value.
@@ -38,5 +72,9 @@ public class CacheManager
 	 * @param path Target path
 	 * @since 1.1
 	 */
-	public void deleteCache(String path)            { cache.removeDefault(path);                }
+	public void deleteCache(String path)
+	{
+		getCache().removeProperty(path);
+		cache.store(cacheFile);
+	}
 }
