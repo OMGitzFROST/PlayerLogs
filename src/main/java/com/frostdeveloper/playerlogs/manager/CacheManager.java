640bba52592948aa4c242bd36608115bd7567278
OMGitzFROST <deltoro.steve98@gmail.com>
2021-12-26 21:55:08 -0800

    Make class more efficient

--- src/main/java/com/frostdeveloper/playerlogs/manager/CacheManager.java
+++ src/main/java/com/frostdeveloper/playerlogs/manager/CacheManager.java
@@ -1,15 +1,13 @@
 package com.frostdeveloper.playerlogs.manager;
 
-import com.frostdeveloper.api.FrostAPI;
+import com.frostdeveloper.api.core.Yaml;
 import com.frostdeveloper.playerlogs.PlayerLogs;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
+import com.frostdeveloper.playerlogs.util.Util;
 
 import java.io.File;
-import java.io.IOException;
 
 /**
- * A class used to manage our cache. This manager includes methods to set, get, and delete cache values.
+ * A class used to manage our cache. This mananger includes methods to set, get, and delete cache values.
  *
  * @author OMGitzFROST
  * @since 1.1
@@ -18,11 +16,11 @@ public class CacheManager
 {
 	// CLASS INSTANCES
 	private final PlayerLogs plugin = PlayerLogs.getInstance();
-	private final FrostAPI api = plugin.getFrostApi();
 	
 	// CLASS SPECIFIC OBJECTS
-	private final File cacheFile = api.toFile( ".cache.yml");
-	
+	private final File cacheFile = Util.toFile( ".cache.yml");
+	private final Yaml cache = new Yaml(cacheFile, true);
+
 	/**
 	 * A method used to set a cache to a value
 	 *
@@ -30,18 +28,8 @@ public class CacheManager
 	 * @param value Target value
 	 * @since 1.1
 	 */
-	public void setCache(String path, Object value)
-	{
-		try {
-			FileConfiguration config = YamlConfiguration.loadConfiguration(cacheFile);
-			config.set(path, value);
-			config.save(cacheFile);
-		}
-		catch (IOException ex) {
-			ex.printStackTrace();
-		}
-	}
-	
+	public void setCache(String path, Object value) { cache.setDefault(path, value, true);      }
+
 	/**
 	 * A method used to get a cache value/
 	 *
@@ -49,26 +37,13 @@ public class CacheManager
 	 * @return Cache value
 	 * @since 1.1
 	 */
-	public String getCache(String path)
-	{
-		if (cacheFile.exists()) {
-			FileConfiguration config = YamlConfiguration.loadConfiguration(cacheFile);
-			return config.getString(path);
-		}
-		else {
-			return null;
-		}
-	}
-	
+	public String getCache(String path)             { return cache.getConfig().getString(path); }
+
 	/**
 	 * A method used to delete a cache path, in other words, used to reset its value.
 	 *
 	 * @param path Target path
 	 * @since 1.1
 	 */
-	public void deleteCache(String path)
-	{
-		FileConfiguration config = YamlConfiguration.loadConfiguration(cacheFile);
-		config.set(path, null);
-	}
+	public void deleteCache(String path)            { cache.removeDefault(path);                }
 }
