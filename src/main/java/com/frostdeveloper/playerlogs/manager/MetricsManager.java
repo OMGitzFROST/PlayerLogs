c947c2b61e486726b58522e2d4fe2523fde186ce
OMGitzFROST <deltoro.steve98@gmail.com>
2021-12-27 19:33:36 -0800

    Major updates for v1.2

--- src/main/java/com/frostdeveloper/playerlogs/manager/MetricsManager.java
+++ src/main/java/com/frostdeveloper/playerlogs/manager/MetricsManager.java
@@ -1,8 +1,8 @@
 package com.frostdeveloper.playerlogs.manager;
 
-import com.frostdeveloper.api.FrostAPI;
-import com.frostdeveloper.playerlogs.PlayerLogs;
 import com.frostdeveloper.playerlogs.definition.Config;
+import com.frostdeveloper.playerlogs.model.Module;
+import com.frostdeveloper.playerlogs.model.Scheduler;
 import org.bstats.bukkit.Metrics;
 import org.bstats.charts.SimplePie;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -14,97 +14,141 @@ import org.bukkit.scheduler.BukkitTask;
  * @author OMGitzFROST
  * @since 1.0
  */
-public class MetricsManager
+public class MetricsManager extends ModuleManager implements Module, Scheduler
 {
 	// CLASS INSTANCES
-	private final PlayerLogs plugin = PlayerLogs.getInstance();
 	private final ConfigManager config = plugin.getConfigManager();
 	private final CacheManager cache = plugin.getCacheManager();
-	private final FrostAPI api = plugin.getFrostApi();
 	
-	// CLASS SPECIFIC OBJECTS
+	private final String identifier = "metric-module";
+	private final String cacheIdentifier = "metric-timer";
+	private final Config permission = Config.USE_METRICS;
 	private static BukkitTask task;
-	int id = 13598;
 	
 	/**
-	 * A method used to initialize our metrics collection, it will gather various metrics
-	 * used to better develop our plugin.
-	 *
-	 * @since 1.0
+	 * {@inheritDoc}
+	 * @since 1.2
 	 */
-	public void runTask()
+	@Override
+	public void initialize() { start(); }
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 1.2
+	 */
+	@Override
+	public void start()
 	{
-		String cachedTimer = cache.getCache("update-timer");
+		String cachedTimer = cache.getCache(cacheIdentifier) != null ? cache.getCache(cacheIdentifier) : "0";
 		
-		task = new BukkitRunnable() {
-			int counter = cachedTimer != null ? Integer.parseInt(cachedTimer) : 0;
-			final int interval = api.toMinute(30);
+		task  = new BukkitRunnable() {
+			int counter = Integer.parseInt(cachedTimer);
 			
 			@Override
 			public void run() {
+				final int interval = api.toMinute(30);
+				
 				// STOP TASK IN-CASE THE DATA FOLDER IS DELETED
-				if (!plugin.getDataFolder().exists()) {
-					this.cancel();
+				if (!plugin.getDataFolder().exists() || !isRegistered()) {
+					shutdown();
 					return;
 				}
 				
 				// IF COUNTER IS GREATER THAN INTERVAL, DELETE CACHE.
 				if (counter > interval) {
-					cache.deleteCache("metrics-timer");
+					cache.deleteCache(cacheIdentifier);
 				}
 				
 				// IF COUNTER IS LESS THAN INTERVAL, ADD TO COUNTER AND SET CACHE
 				if (counter < interval) {
 					counter++;
-					cache.setCache("metrics-timer", counter);
+					cache.setCache(cacheIdentifier, counter);
 				}
 				else {
 					// IF ALL CHECKS PASS, ATTEMPT UPDATE AND RESET CACHE
+					int id = 13598;
+					Metrics metrics = new Metrics(plugin, id);
 					
-					if (config.getBoolean(Config.USE_METRICS)) {
-						Metrics metrics = new Metrics(plugin, id);
-						
-						metrics.addCustomChart(new SimplePie("locale", () -> config.getString(Config.LOCALE)));
-						metrics.addCustomChart(new SimplePie("version", () -> config.getString(Config.VERSION)));
-						metrics.addCustomChart(new SimplePie("auto_updater", () -> config.getString(Config.AUTO_UPDATE)));
-						metrics.addCustomChart(new SimplePie("use_prefix", () -> config.getString(Config.USE_PREFIX)));
-						metrics.addCustomChart(new SimplePie("use_custom_message", () -> config.getString(Config.CUSTOM_MESSAGE)));
-						
-						plugin.log(MetricsManager.class, "metrics.collect.success");
-					}
-					else {
-						plugin.log(MetricsManager.class, "metrics.collect.disabled");
-					}
+					metrics.addCustomChart(new SimplePie("locale", () -> config.getString(Config.LOCALE)));
+					metrics.addCustomChart(new SimplePie("auto_updater", () -> config.getString(Config.AUTO_UPDATE)));
+					metrics.addCustomChart(new SimplePie("use_prefix", () -> config.getString(Config.USE_PREFIX)));
+					metrics.addCustomChart(new SimplePie("use_custom_message", () -> config.getString(Config.CUSTOM_MESSAGE)));
+					
+					plugin.log(getClass(), "metrics.collect.success");
 					
 					counter = 0;
-					cache.setCache("metrics-timer", counter);
+					cache.setCache(cacheIdentifier, counter);
 				}
 			}
 		}.runTaskTimer(plugin, 0, 20);
 	}
 	
 	/**
-	 * A method used to stop our updater task, this method will stop the current scheduler, setting it up for a successful
-	 * server state change.
-	 *
-	 * @since 1.1
+	 * {@inheritDoc}
+	 * @since 1.2
 	 */
-	public void stopTask()
+	@Override
+	public void registerModule()
 	{
-		if (getTask().isCancelled()) {
-			plugin.log("metrics.disable.unchanged");
-		}
-		else {
-			getTask().cancel();
-			plugin.debug("metrics.task.disabled");
+		addToMaster(this);
+		
+		if (config.getBoolean(permission)) {
+			addToRegistry(this);
 		}
 	}
 	
 	/**
-	 * A method used to return an instance of our updater task. This allows us to modify our task.
-	 *
-	 * @return Our updater task.
-	 * @since 1.0
+	 * {@inheritDoc}
+	 * @since 1.2
 	 */
-	public BukkitTask getTask() { return task; }
+	@Override
+	public void shutdown()
+	{
+		cancel();
+		
+		if (isCancelled()) {
+			plugin.debug(getClass(), "module.unload.success", identifier);
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @return Module registry status
+	 * @since 1.2
+	 */
+	@Override
+	public boolean isRegistered() { return getRegisteredList().contains(this); }
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 1.2
+	 */
+	public String getIdentifier() { return identifier;                         }
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 1.2
+	 */
+	@Override
+	public int getTaskId() { return getRegisteredList().indexOf(this); }
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 1.2
+	 */
+	@Override
+	public boolean isCancelled() { return task == null || task.isCancelled(); }
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 1.2
+	 */
+	@Override
+	public void cancel()
+	{
+		if (task != null) {
+			task.cancel();
+		}
+	}
 }
