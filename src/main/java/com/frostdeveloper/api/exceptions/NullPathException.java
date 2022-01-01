084b16c5bff1d73ca8021fe6233dc4327005294d
OMGitzFROST <deltoro.steve98@gmail.com>
2021-12-31 17:14:18 -0800

    Added Custom Exception

---
+++ src/main/java/com/frostdeveloper/api/exceptions/NullPathException.java
@@ -0,0 +1,8 @@
+package com.frostdeveloper.api.exceptions;
+
+public class NullPathException extends NullPointerException
+{
+	public NullPathException() { super(); }
+	
+	public NullPathException(String message) { super(message); }
+}
