fc3a153dff08a70f69ae85be8521748fcc066014
OMGitzFROST <deltoro.steve98@gmail.com>
2022-01-18 00:27:42 -0800

    Deleted class

--- src/main/java/com/frostdeveloper/api/exceptions/NullPathException.java
+++
@@ -1,8 +0,0 @@
-package com.frostdeveloper.api.exceptions;
-
-public class NullPathException extends NullPointerException
-{
-	public NullPathException() { super(); }
-	
-	public NullPathException(String message) { super(message); }
-}
--- src/main/java/com/frostdeveloper/api/exceptions/UndefinedFileException.java
+++
@@ -1,8 +0,0 @@
-package com.frostdeveloper.api.exceptions;
-
-public class UndefinedFileException extends IllegalArgumentException
-{
-	public UndefinedFileException()              { super();        }
-	
-	public UndefinedFileException(String message){ super(message); }
-}
