c961931654de2e6beca31eeeec1ba86546e0fcf5
OMGitzFROST <deltoro.steve98@gmail.com>
2022-01-19 21:49:39 -0800

    Module Rework

--- src/main/java/com/frostdeveloper/playerlogs/module/QuitModule.java
+++ src/main/java/com/frostdeveloper/playerlogs/module/QuitModule.java
@@ -1,9 +1,9 @@
 package com.frostdeveloper.playerlogs.module;
 
 import com.frostdeveloper.playerlogs.definition.Config;
-import com.frostdeveloper.playerlogs.manager.ModuleManager;
+import com.frostdeveloper.playerlogs.definition.Variable;
 import com.frostdeveloper.playerlogs.model.Module;
-import com.frostdeveloper.playerlogs.util.Util;
+import com.frostdeveloper.playerlogs.util.Placeholder;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,25 +11,36 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
-import java.util.Objects;
-
 /**
- * A method used to handle our quit module
- *
+ * This module class houses all required methods inorder for this module to work, Each module is
+ * nested under our {@link Module} class that defines all required methods needed for a module to
+ * work.
  *
  * @author OMGitzFROST
  * @since 1.0
  */
-public class QuitModule extends ModuleManager implements Module, Listener
+public class QuitModule extends Module implements Listener
 {
 	// CLASS SPECIFIC OBJECTS
-	private final String identifier = "quit-module";
-	private final Config permission = Config.MODULE_QUIT_ENABLED;
-	private final Config message    = Config.MODULE_QUIT_MSG;
+	private final Config enabled = Config.MODULE_QUIT_ENABLED;
+	private final Config message  = Config.MODULE_QUIT_MSG;
 	
 	/**
-	 * {@inheritDoc}
+	 * An event handler used to handle our quit event when triggered and execute the required tasks
+	 *
+	 * @param event Event triggered
+	 * @since 1.0
+	 */
+	@EventHandler
+	public void onPlayerQuit(@NotNull PlayerQuitEvent event)
+	{
+		Player player = event.getPlayer();
+		Placeholder.addCustom(Variable.DEFAULT, event.getQuitMessage());
+		printToFile(player,  Placeholder.set(player, getMessage()));
+	}
+	
+	/**
+	 * A method is called once the module is registered, and initializes the assigned arithmetic.
 	 *
 	 * @since 1.2
 	 */
@@ -37,60 +48,63 @@ public class QuitModule extends ModuleManager implements Module, Listener
 	public void initialize() { Bukkit.getServer().getPluginManager().registerEvents(this, plugin); }
 	
 	/**
-	 * An event handler used to listen for when a player leaves the server and log the event
-	 * to a file.
+	 * A method used to return the message assigned to a module
 	 *
-	 * @param event Triggered event
-	 * @since 1.1
-	 */
-	@EventHandler
-	public void onPlayerQuit(@NotNull PlayerQuitEvent event)
-	{
-		Player player   = event.getPlayer();
-		File playerFile = Util.toFile(getPlayerDir(player), "{0}.log", identifier);
-		File[] logFiles = {globalFile, playerFile};
-		
-		boolean requireDefault = Objects.requireNonNull(getConfig().getString(message.getPath())).equalsIgnoreCase("DEFAULT");
-		String msg = requireDefault ? event.getQuitMessage() : Objects.requireNonNull(getConfig().getString(message.getPath()));
-		
-		if (getConfig().getBoolean(Config.MODULARIZE.getPath())) {
-			printToFile(logFiles, api.stripColor(msg));
-		}
-		else {
-			printToFile(globalFile, api.stripColor(msg));
-		}
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 *
+	 * @return Module message
 	 * @since 1.2
 	 */
 	@Override
-	public void registerModule()
-	{
-		addToMaster(this);
-		
-		if (getConfig().getBoolean(permission.getPath())) {
-			addToRegistry(this);
-		}
-	}
+	public String getMessage() { return manager.getString(message); }
 	
 	/**
-	 * {@inheritDoc}
+	 * A method used to return whether a module is enabled
+	 *
+	 * @return Module status
+	 * @since 1.2
+	 */
+	@Override
+	public boolean isEnabled() { return manager.getBoolean(enabled); }
+	
+	/**
+	 * A method used to determine whether a module is registered.
 	 *
 	 * @return Module registry status
 	 * @since 1.2
 	 */
 	@Override
-	public boolean isRegistered()          { return getRegisteredList().contains(this); }
+	public boolean isRegistered() { return  manager.getRegisteredList().contains(this); }
 	
 	/**
-	 * {@inheritDoc}
+	 * A method used to return the identifier for a module. The identifier serves as the name of
+	 * the module and additionally can be used to track its timer using the cache manager.
 	 *
 	 * @return Module identifier
 	 * @since 1.0
 	 */
+	public @NotNull String getIdentifier()
+	{
+		String rawModuleName = this.getClass().getSimpleName().toLowerCase();
+		return rawModuleName.replace("module", "");
+	}
+	
+	/**
+	 * A method used to return the full identifier for a module.
+	 *
+	 * @return Full module identifier
+	 * @since 1.2
+	 */
 	@Override
-	public @NotNull String getIdentifier() { return identifier;                         }
+	public @NotNull String getFullIdentifier()
+	{
+		String rawModuleName = this.getClass().getSimpleName().toLowerCase();
+		return rawModuleName.replace("module", "-module");
+	}
+	
+	/**
+	 * A method used to return the active handler list for a module.
+	 * 
+	 * @since 1.2
+	 */
+	@Override
+	public void removeListener() { PlayerQuitEvent.getHandlerList().unregister(this); }
 }
